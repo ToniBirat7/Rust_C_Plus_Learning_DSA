4fd4c4b2c7b2befb3ebcd428a1a8864a0f28218e
ToniBirat7 <gotofreelancing1@gmail.com>
2025-04-11 17:20:14 +0545

    Day10

--- C++_With_DSA/Apna_College_Basics/Day10.cpp
+++ C++_With_DSA/Apna_College_Basics/Day10.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 vector<int> pairSumBruteForce(vector<int>& nums, int target) {
@@ -18,14 +19,13 @@ vector<int> pairSumBruteForce(vector<int>& nums, int target) {
     cout << "Right : " << right << " Left " << left << endl;
 
     for(left; left <= right; left++) {
-        for(int j = left + 1; j <= right; j++) {
-            if(nums[left] + nums[j] == target) {
-                pairs.push_back(nums[left]);
-                pairs.push_back(nums[j]);
-            }
-        }
+		for(int j = left + 1; j <= right; j++) {
+			if(nums[left] + nums[j] == target) {
+					pairs.push_back(nums[left]);
+					pairs.push_back(nums[j]);
+			}
+		}
     }
-
     return pairs;
 } 
 
@@ -59,56 +59,92 @@ vector<int> pairSumOptimized(vector<int>& nums, int target) {
 int majorityElementBruteForce(vector<int>& arr2) {
 	
 	int maxCount = arr2.size() / 2;	
-	int element;
 
 	cout << "Max " << maxCount << endl;
 
-	for(int i = 0; i < arr2.size(); i++) {
-		int currCount = 1;
-		for(int j = i + 1; j < arr2.size(); j++) {
-			if(arr2[i] == arr2[j]) {
-				currCount++;
+	for(int i : arr2) {
+		int freq = 0;
+		for(int j : arr2) {
+			if(i == j) {
+				freq++;
 			}
 		}
-		if(currCount > maxCount) {
-			element = arr2[i];
-			break;
+		if(freq > maxCount) {
+			return i;
 		}
 	}
-	return element;
+
+	return -1;
+}
+
+
+int majorityElementSort(vector<int>& arr2) {
+	
+	int maxCount = arr2.size() / 2;	
+
+	cout << "Max " << maxCount << endl;
+
+	sort(arr2.begin(), arr2.end());
+
+	int freq = 0;
+	int ans = arr2[0];
+
+	for(int i : arr2) {
+		if(ans == i) {
+			freq++;
+		}
+		else {
+			freq = 1;
+			ans = i;
+		}
+		if(freq > maxCount) {
+			return ans;
+		}
+	}
+	return -1;
 }
 
 int main() {
-    int target = 9;
-    vector<int> arr = {1,2,3,4,5,6,7,8,9,10};
-    
-    vector<int> pairs = pairSumBruteForce(arr, target);
+	int target = 9;
 
-    cout << "Pair Sum Brute Force Approach, paris are below :" << endl;
+	vector<int> arr = {1,2,3,4,5,6,7,8,9,10};
+	
+	vector<int> pairs = pairSumBruteForce(arr, target);
 
-    for(int num : pairs) {
-      cout << num << " ";
-    }
+	cout << "Pair Sum Brute Force Approach, paris are below :" << endl;
 
-    cout << endl;
+	for(int num : pairs) {
+		cout << num << " ";
+	}
 
-    cout << "Pair Sum Optimized Approach, paris are below :" << endl;
+	cout << endl;
 
-    vector<int> pairsOptimized = pairSumOptimized(arr, target);
+	cout << "Pair Sum Optimized Approach, paris are below :" << endl;
 
-    for(int num : pairsOptimized) {
-      cout << num << " ";
-    }
+	vector<int> pairsOptimized = pairSumOptimized(arr, target);
 
-    cout << endl;
+	for(int num : pairsOptimized) {
+		cout << num << " ";
+	}
 
-		vector<int> arr2 = {2,2,1,1,1,2,2};
+	cout << endl;
 
-		cout << "Majority Element Brute Force Approach, below is the number:" << endl;
+	vector<int> arr2 = {2,2,1,1,1,2,2};
 
-    int element = majorityElementBruteForce(arr2);
+	cout << "Majority Element Brute Force Approach, below is the number:" << endl;
 
-		cout << element;
+	int element = majorityElementBruteForce(arr2);
 
-    cout << endl;
+	cout << element;
+
+	cout << endl;
+
+	cout << "Majority Element Sort Approach, below is the number:" << endl;
+
+	int elementSort = majorityElementSort(arr2);
+
+	cout << elementSort;
+
+	cout << endl;
+	
 }
