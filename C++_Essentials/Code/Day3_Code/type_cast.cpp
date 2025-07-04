c37b4a4825bdc8141b1347b7907c5eb232ce5573
ToniBirat7 <gotofreelancing1@gmail.com>
2025-07-04 12:56:49 +0545

    docs: type_casting completed

--- C++_Essentials/Code/Day3_Code/type_cast.cpp
+++ C++_Essentials/Code/Day3_Code/type_cast.cpp
@@ -17,7 +17,16 @@ void self_cast()
 
   sprite_x = target_x; // Will convert into integer
 
-  player_x = sprite_x; // Will not be able to assign a negative number in an unsigned variable
+  player_x = sprite_x; // Will not be able to assign a negative number in an unsigned variable, we will get unexpected value of 2's compliment
+
+  cout << "Target X (float) " << target_x << endl;
+
+  cout << "Sprite X (int) " << sprite_x << endl;
+
+  cout << "Player X (uint) " << player_x << endl;
+
+  // But if we cast the value then we will get the expected output
+  cout << "Player X (uint) after conversion to signed 32 bit int " << static_cast<int32_t>(player_x) << endl;
 }
 
 int main()
