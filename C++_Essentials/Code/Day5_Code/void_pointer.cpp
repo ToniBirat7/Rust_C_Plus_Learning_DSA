ef99da101835653d00e7346f4a20657c3856c3ce
ToniBirat7 <gotofreelancing1@gmail.com>
2025-07-06 12:27:20 +0545

    docs: void pointers complete

---
+++ C++_Essentials/Code/Day5_Code/void_pointer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  float playerHealth = 100.0f;          // Player's health in a game
+  void *genericPointer = &playerHealth; // Generic pointer pointing to player's health
+
+  // To dereference, cast the pointer back to float*
+  float actualHealth = *(static_cast<float *>(genericPointer));
+
+  // Or alternatively
+  float enemyHealth = *(float *)genericPointer;
+}
