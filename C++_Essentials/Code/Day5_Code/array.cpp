7e1e09770986d67bf42a70a43acf94e7728f61b5
ToniBirat7 <gotofreelancing1@gmail.com>
2025-07-06 10:34:13 +0545

    docs: arr completed

--- C++_Essentials/Code/Day5_Code/array.cpp
+++ C++_Essentials/Code/Day5_Code/array.cpp
@@ -4,8 +4,10 @@ using namespace std;
 int main()
 {
   int enemies[4];
+  const float levelDifficulty[] = {1.0, 2.0, 3.0};         // It will be stored as double
+  const float floatLevelDifficulty[] = {1.0f, 2.0f, 3.0f}; // It will be stored as float
 
-  enemies[0] = 4;
+  enemies[0] = 4; // If we've declared const we cannot assign value
   enemies[1] = 5;
   enemies[2] = 6;
   enemies[3] = 7;
