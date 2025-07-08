eda395e2d22a106af22e2be71d3297981201cf90
ToniBirat7 <gotofreelancing1@gmail.com>
2025-07-08 12:25:12 +0545

    docs: reference in c++

--- C++_Essentials/Code/Day6_Code/reference.cpp
+++ C++_Essentials/Code/Day6_Code/reference.cpp
@@ -2,10 +2,25 @@
 #include <vector>
 using namespace std;
 
+void implementReference()
+{
+  int age = 20;
+
+  int &trackAge = age;
+
+  cout << "Address of A is : " << &age << endl;
+  cout << "Address of B is : " << &trackAge << endl;
+
+  trackAge = 30;
+
+  cout << "Value of A after is : " << age << endl;
+  cout << "Value of B after is : " << trackAge << endl;
+}
+
 void check_ref()
 {
 
-  // Ref check with non-premitive D Type
+  // Ref check with non-premitive D Type array
   int profiles[] = {1, 2, 3, 4};
   // int secondProfiles[] = profiles; // We cannot directly assign
   // an array to another variable
@@ -14,8 +29,17 @@ void check_ref()
   int a = 10;
   int b = a;
 
+  // Ref check with non-primitive D Type vector
+
+  vector<int> points = {1, 2, 3};
+
+  vector<int> secondPoints = points;
+
   cout << "Address of A is : " << &a << endl;
   cout << "Address of B is : " << &b << endl;
+
+  cout << "Address of first Vect is : " << &points << endl;
+  cout << "Address of second Vect is : " << &secondPoints << endl;
 }
 
 // Vector for complex numbers
@@ -23,5 +47,13 @@ int main()
 {
   // Check ref
   check_ref();
+
+  cout << "Implementation of Reference" << endl;
+
+  cout << endl;
+
+  // Reference implementation
+  implementReference();
+
   return 0;
 }
