ca6863142be99b28b5150a12323f234692f7d2a4
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-03 18:18:28 +0545

    docs: inheritance and polymorphism

--- C++_Essentials/Code/Day13_Code/include/Valuable.h
+++ C++_Essentials/Code/Day13_Code/include/Valuable.h
@@ -6,4 +6,11 @@ class Valuable : public Item
 {
 private:
   int value;
-}
+
+public:
+  Valuable(const std::string &valuableName, int valuableWeight, int valuableValue);
+
+  void use() const override;
+
+  void display() const override;
+};
