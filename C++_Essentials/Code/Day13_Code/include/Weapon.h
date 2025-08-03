a17bd32280d9e4a499824927c797644e7f320bea
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-03 15:05:02 +0545

    docs: inheritance and polymorphism

--- C++_Essentials/Code/Day13_Code/include/Weapon.h
+++ C++_Essentials/Code/Day13_Code/include/Weapon.h
@@ -1,2 +1,19 @@
 #pragma once
+#include "Item.h" // Because Weapon Inhertis Item
 
+class Weapon : public Item
+{
+private:
+  int damage;
+
+public:
+  // Constructor
+  Weapon(const std::string &weaponName, int weaponWeight, int weaponDamage);
+
+  // Override the use Method
+  void use() const override;
+
+  // Override the display method
+
+  void display() const override;
+};
