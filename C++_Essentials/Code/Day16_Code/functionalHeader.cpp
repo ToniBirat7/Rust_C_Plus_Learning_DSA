3d9e03b5145ec2697b1373e6a03f2104eafd3874
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-06 09:42:28 +0545

    docs: functors

--- C++_Essentials/Code/Day16_Code/functionalHeader.cpp
+++ C++_Essentials/Code/Day16_Code/functionalHeader.cpp
@@ -6,7 +6,7 @@
 int main()
 {
   std::vector<int> scores = {10, 40, 20, 50, 30};
-  std::sort(scores.begin(), scores.end(), std::greater<int>());
+  std::sort(scores.begin(), scores.end(), std::greater<int>()); // It is a functor used to sort a vector in decending order.
   for (const auto &score : scores)
     std::cout << "Score: " << score << std::endl;
   return 0;
